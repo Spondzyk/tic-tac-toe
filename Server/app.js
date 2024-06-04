cedc1f260653026263fcd9e3d83bebe7df2ab739
Spondzyk <mikolajgabka@gmail.com>
2024-06-04 18:02:06 +0200

    cognito

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,9 @@
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const {saveGameResult} = require("./database_connection");
+const {handleDatabaseOperation} = require("./database_connection");
 const jwkToPem = require('jwk-to-pem');
 const jsonwebtoken = require('jsonwebtoken');
+const jsonWebKeys = require('./aws_keys');
 
 const server_address = "http://" + process.env.FRONTEND_GLOBAL_IP + ":8080"
 const httpServer = createServer();
@@ -14,24 +15,6 @@ const allUsers = {};
 const allRooms = [];
 
 let isValid = false;
-
-const jsonWebKeys = [
-  {
-    "alg": "RS256",
-    "e": "AQAB",
-    "kid": "E0eA/upedKgiHiTkkDcpWIaEqR1FE0ZCmwmo+mRx/jg=",
-    "kty": "RSA",
-    "n": "2Te-Mkebq0rxc-VFa7CgxD7ZjAMUayd2tpJBxGWcErSShGqPC6BRvxncMW_5GSaupQlbmmJxwqiDvGezfaoSaVQF5sVbXyMYtuqA3uQ4Uu7U2b1k0JK5-kAiNHAXt63beHVOcu42KehwGmVL1sxoBDPT_hitpe8rKRKTs9d3OXyXVmA_KTQuZyxbO9WInRJck66nYo_Ym8XuQNpVE-fPKsGX4X_0R6hIXnGYF8W3NzAUZhk14uX5JfHCeDNbuZiKUS0uLsFU98fC0DUhOSrxN-g_VFnA1gBdJhaLkPsGxKE9nntJG7UFUcYnyFns0s3W3YrYgJKr_mPpN3wrs57zAw",
-    "use": "sig"
-  }, {
-    "alg": "RS256",
-    "e": "AQAB",
-    "kid": "UdPsVYDEkX45+DRnisZZ4yoeoMPp+pKhuIlI/CUXXF4=",
-    "kty": "RSA",
-    "n": "yykFsr6_xmdItmyz_byMncCrqHYfyNyf5aUff2O72tD_uYfdgB91TEdrvaaDZS5wkTt7kkZyMIDJwTQXwhk8RdPquw_FdsFp5iVbcBYFkT6sUMthj6KtrJ56NjGLjiGycnhwQ0j-Rqh1SOyF3la2RDseMWN0QSm6bfleqLpqe2sIq7Yc23llihW_5Mdi_DhYvxRLLhfSpYozNtCBQVgWDCiNo7ZlT31iLG2GLqUdPZtbwx86mVJrifB_klpF8iM5FHf-VvbfETpRzxe05kJke8Fsrg8wBdR_5z_BBLnPpA66bcQ-vPpK23xy7VfEqntZv7PhgzmtnVY8YF1q-nAG0w",
-    "use": "sig"
-  }
-]
 
 function decodeTokenHeader(token) {
   const [headerEncoded] = token.split('.');
@@ -158,9 +141,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("end_game", (data) => {
-    console.log(data);
-
-    saveGameResult(data.player1, data.player2, data.winner);
+    handleDatabaseOperation(data.player1, data.player2, data.winner).then(r => console.log(data));
   });
 });
 
